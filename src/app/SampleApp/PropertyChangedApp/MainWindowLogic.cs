750d31aedbf00053362068694f99dc12c683fe87
mealho <mealhosoft@gmail.com>
2021-10-03 21:55:38 +0900

    PropertyChanged

---
+++ src/app/SampleApp/PropertyChangedApp/MainWindowLogic.cs
@@ -0,0 +1,25 @@
+﻿using MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyChangedApp
+{
+    class MainWindowLogic : ModelBase
+    {
+        public string StatusMessage {
+            get
+            {
+                return statusMessage;
+            }
+            set
+            {
+                statusMessage = value;
+                OnPropertyChanged(nameof(StatusMessage));
+            }
+        }
+        private string statusMessage;
+    }
+}
